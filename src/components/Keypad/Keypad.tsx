a6366377127dbe15345961bfdec211794e60d775
fabcodingzest <fabrizvi786@gmail.com>
2023-01-20 04:46:31 +0530

    adding backspace icon

--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -1,7 +1,9 @@
+import { ReactComponent as BackSpace } from '../../assets/backspace.svg'
+
 const keyboardLetters = [
   ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
   ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-  ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Clear'],
+  ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Backspace'],
 ]
 
 const Keypad = () => {
@@ -12,12 +14,12 @@ const Keypad = () => {
           <div key={i} className='flex justify-center gap-x-1 pb-1 md:gap-2 md:pb-2'>
             {row.map((letter) => {
               return (
-                <div
+                <button
                   className='bg-grey-med border-grey-dark flex h-6 w-min items-center justify-center rounded-md border px-2 text-xs md:h-10 md:px-3 md:text-sm '
                   key={letter}
                 >
-                  {letter}
-                </div>
+                  {letter === 'Backspace' ? <BackSpace /> : letter}
+                </button>
               )
             })}
           </div>
