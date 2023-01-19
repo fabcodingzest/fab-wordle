79e081d47511d23d3d96384de4d79f0813f0d060
fabcodingzest <fabrizvi786@gmail.com>
2023-01-20 04:36:08 +0530

    coded keypad with responsive styles

--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -1,6 +1,29 @@
+const keyboardLetters = [
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Clear'],
+]
+
 const Keypad = () => {
   return (
-    <div>Keypad</div>
+    <section>
+      {keyboardLetters.map((row, i) => {
+        return (
+          <div key={i} className='flex justify-center gap-x-1 pb-1 md:gap-2 md:pb-2'>
+            {row.map((letter) => {
+              return (
+                <div
+                  className='bg-grey-med border-grey-dark flex h-6 w-min items-center justify-center rounded-md border px-2 text-xs md:h-10 md:px-3 md:text-sm '
+                  key={letter}
+                >
+                  {letter}
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+    </section>
   )
 }
 
