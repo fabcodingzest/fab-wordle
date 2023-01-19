37fbe644f6f25c26e86b5827399670df2a3556f9
fabcodingzest <fabrizvi786@gmail.com>
2023-01-20 02:17:42 +0530

    added palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          dark: '#5964E0',
+          light: '#939BF4',
+        },
+        blue: {
+          dark: '#19202D',
+          midnight: '#121721',
+        },
+
+        grey: {
+          light: '#F4F6F8',
+          med: '#9DAEC2',
+          dark: '#6E8098',
+          btn: '#303642',
+          hover: '#525861',
+        },
+      },
+    },
   },
   plugins: [],
 }
