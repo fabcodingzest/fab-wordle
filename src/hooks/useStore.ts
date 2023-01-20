fde17fb11b1d77f630f40b0d6814b9d5791c3ddd
fabcodingzest <fabrizvi786@gmail.com>
2023-01-20 13:14:25 +0530

    refactored and added some comments

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -3,6 +3,7 @@ import toast from 'react-hot-toast'
 import dictionary from '../utility/dictionary.json'
 import 'react-responsive-modal/styles.css'
 import { TileProps } from '../App'
+import { green, unused, used, yellow } from '../utility/constants'
 
 const useStore = (solution: string) => {
   const [turn, setTurn] = useState(0)
@@ -21,77 +22,77 @@ const useStore = (solution: string) => {
 
   const formatGuess = () => {
     const solArr: (string | null)[] = [...solution]
-    const formattedGuess = [...currentGuess].map((letter) => ({ letter, color: 'bg-grey-med' }))
+    const formattedGuess = [...currentGuess].map((letter) => ({ letter, color: unused }))
 
     // set green color for right guesses
     formattedGuess.forEach((item, i) => {
       if (solArr[i] === item.letter) {
-        formattedGuess[i].color = 'bg-green-400'
+        formattedGuess[i].color = green
         solArr[i] = null
       }
     })
 
     // set yellow for letters that are on wrong place and havent
     formattedGuess.forEach((item, i) => {
-      if (solArr.includes(item.letter) && item.color !== 'bg-green-400') {
-        formattedGuess[i].color = 'bg-yellow-400'
+      if (solArr.includes(item.letter) && item.color !== green) {
+        formattedGuess[i].color = yellow
         solArr[solArr.indexOf(item.letter)] = null
       }
     })
     return formattedGuess
   }
+  const addNewWord = (guess: string) => {
+    if (history.includes(guess)) {
+      toast('Word already guessed', { duration: 3000 })
+      return
+    }
+    const formattedGuess = formatGuess()
+    setGuesses((prev) => {
+      const newGuesses = [...prev]
+      newGuesses[turn] = formattedGuess
+      return newGuesses
+    })
+    setHistory((prev) => [...prev, guess])
+    setUsedKeys((prev) => {
+      const newKeys = { ...prev }
+      formattedGuess.forEach((item) => {
+        const currentColor = newKeys[item.letter]
 
+        if (item.color === green) {
+          newKeys[item.letter] = green
+          return
+        }
+        if (item.color === yellow && currentColor !== green) {
+          newKeys[item.letter] = yellow
+          return
+        }
+        if (item.color === unused && currentColor !== green && currentColor !== yellow) {
+          newKeys[item.letter] = used
+          return
+        }
+      })
+      return newKeys
+    })
+  }
   const checkSubmission = (guess: string) => {
     // if dictionary have this word then go forward
     if (dictionary.includes(guess)) {
       // if the word is same as solution
       if (solution === guess) {
-        console.log(solution === guess)
         setIsCorrect(true)
         toast.success(solution, { duration: 2500 })
         setTimeout(() => setOpen(true), 3000)
       }
-      // format the currentGuess to detailed object array
-      if (history.includes(guess)) {
-        toast('Word already guessed', { duration: 3000 })
-        return
-      }
-      const formattedGuess = formatGuess()
-      setGuesses((prev) => {
-        const newGuesses = [...prev]
-        newGuesses[turn] = formattedGuess
-        return newGuesses
-      })
-      setHistory((prev) => [...prev, guess])
-      setUsedKeys((prev) => {
-        const newKeys = { ...prev }
-        formattedGuess.forEach((item) => {
-          const currentColor = newKeys[item.letter]
-
-          if (item.color === 'bg-green-400') {
-            newKeys[item.letter] = 'bg-green-400'
-            return
-          }
-          if (item.color === 'bg-yellow-400' && currentColor !== 'bg-green-400') {
-            newKeys[item.letter] = 'bg-yellow-400'
-            return
-          }
-          if (
-            item.color === 'bg-grey-med' &&
-            currentColor !== 'bg-green-400' &&
-            currentColor !== 'bg-yellow-400'
-          ) {
-            newKeys[item.letter] = 'bg-grey-darkest'
-            return
-          }
-        })
-        return newKeys
-      })
+      // add new word in history, guesses and colors to usedKeys for keyboard
+      addNewWord(guess)
+      // format currentGuess
       setCurrentGuess('')
-      if (turn === 5) {
+      // on last turn if the current guess is wrong
+      if (turn === 5 && !isCorrect) {
         toast.error(solution, { duration: 1000 })
         setTimeout(() => setOpen(true), 1000)
       }
+      // increase turn after every try
       setTurn((prev) => prev + 1)
     } else {
       // otherwise show toast the word doesn't exist
@@ -101,18 +102,13 @@ const useStore = (solution: string) => {
 
   const handleInput = (key: string) => {
     // if its backspace pop up the letter from current guess
-    console.log(key)
-
     if (key === 'Backspace') {
       setCurrentGuess((prev) => prev.slice(0, -1))
       return
     }
     // if its enter then call word submit function
     else if (key === 'Enter') {
-      if (turn > 5) {
-        // setIsCorrect(true)
-        return
-      } else if (currentGuess.length < 5) {
+      if (currentGuess.length < 5) {
         // if less then 5 then show toast incomplete word or something
         toast('Not enough letters')
       } else {
---
+++ src/utility/constants.ts
@@ -0,0 +1,4 @@
+export const green = 'bg-green-400'
+export const yellow = 'bg-yellow-400'
+export const used = 'bg-grey-darkest'
+export const unused = 'bg-grey-med'
