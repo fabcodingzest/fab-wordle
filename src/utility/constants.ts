04e068480282cd3f897bcac827e9108b183f79f6
fabcodingzest <fabrizvi786@gmail.com>
2023-01-20 14:04:16 +0530

    border bug fix: tailwind gotta have full name dynamic wont work

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,4 +1,4 @@
-export const green = 'bg-green-400'
-export const yellow = 'bg-yellow-400'
+export const green = 'bg-green-400 border-green-400'
+export const yellow = 'bg-yellow-400 border-yellow-400'
 export const used = 'bg-grey-darkest'
 export const unused = 'bg-grey-med'
