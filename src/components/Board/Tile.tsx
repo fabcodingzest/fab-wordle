bc31c710dd83b2fdde9b96986a19e1fc623dc2f5
fabcodingzest <fabrizvi786@gmail.com>
2023-01-20 15:55:10 +0530

    fixed tile border issue

--- src/components/Board/Tile.tsx
+++ src/components/Board/Tile.tsx
@@ -3,7 +3,9 @@ import { TileProps } from '../../App'
 const Tile = ({ letter, color }: TileProps) => {
   return (
     <div
-      className={`border-grey-med flex h-10 w-10 items-center justify-center border-2 dark:text-white ${color}`}
+      className={`${
+        color ? color : 'border-grey-dark'
+      } font-sm text-bold flex h-10 w-10 items-center justify-center border-2 dark:text-white `}
     >
       {letter.toUpperCase()}
     </div>
