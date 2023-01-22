b63ef784f1edea82401b66f8ace9a5db1996d5f8
fabcodingzest <fabrizvi786@gmail.com>
2023-01-23 03:42:44 +0530

    added react type defs

--- src/hooks/useSafeLocalStorage.ts
+++ src/hooks/useSafeLocalStorage.ts
@@ -4,7 +4,7 @@ import { useState } from 'react'
 const useSafeLocalStorage = <T>(
   key: string,
   initialValue: T,
-): [T, (value: T | ((val: T) => T)) => void] => {
+): [T, React.Dispatch<React.SetStateAction<T>>] => {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
@@ -22,7 +22,7 @@ const useSafeLocalStorage = <T>(
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = (value: T | ((val: T) => T)) => {
+  const setValue = (value: T | React.SetStateAction<T>) => {
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore = value instanceof Function ? value(storedValue) : value
